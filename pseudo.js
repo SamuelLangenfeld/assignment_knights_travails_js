6692a7388a00dd3aa36f891cf59226966e46fb5f
Johann <baptistajohann@gmail.com>
2018-01-22 13:04:03 -0800

    done

---
+++ pseudo.js
@@ -0,0 +1,19 @@
+// PSEUDO CODE
+
+// Search node tree using DFS
+
+// initialize a stack
+// start at the root node, push it on the stack
+// while stack length > 0
+// pop from stack, check if searched
+// if so, 
+//   do nothing
+// if not,
+//   check if correct value,
+//   if so,
+//     return value
+//   if not 
+//      label "searched" and push children onto stack
+// console log "value not found"
+
+// 
